e25f38181038a0ed88886ea21709c2e82b857122
Hugo Alves <goninja@gmail.com>
2014-12-13 19:59:44 +0000

    com o token do instagram como env variable, tb já posta no heroku

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -2,8 +2,8 @@ require "instagram"
 
 Instagram.configure do |config|
 
-config.client_id = "dda31f9abd884fb38fc7471792f646c0"
+config.client_id = ENV["INSTA_ID"]
 
-config.access_token = "11056439.dda31f9.8283e081877645f185d2f3dd9d61825a"
+config.access_token = ENV["INSTA_TOKEN"]
 
 end
