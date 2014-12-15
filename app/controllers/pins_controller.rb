b3fccbe849adda460e6d769f700c5ea91c274fd3
Hugo Alves <goninja@gmail.com>
2014-12-15 08:51:35 +0000

    com os dados do user a sair no index mas por acabar

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,16 +1,14 @@
 class PinsController < ApplicationController
 
 	def index
-	    @instagram = Instagram.user_recent_media(params[:q], {:count => 10})
+		teste = Instagram.user_search(params[:q], {:count => 1})
+		@instagram = Instagram.user_recent_media(teste, {:count => 5})
 	end
 
 	def perfil
 		@instagram = Instagram.user_search(params[:q], {:count => 1})
 	end
 
-	def busca
-		@instagram = Instagram.user_search(:query, {:count => 1})
-	end
 
 end
 
