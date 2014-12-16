b66c96efb1027edee03692e430fedb86ba78676c
hugo.alves <goninja@gmail.com>
2014-12-16 16:05:13 +0000

    com o index a fazer busca das ultimas fotos do username

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,11 +1,12 @@
 class PinsController < ApplicationController
 
 	def index
-		#@teste = Instagram.user_search(params[:q], {:count => 1}).to_json
-		#@teste = ActiveSupport::JSON.decode(@teste)
-		#@userid = @teste[:id].to_i
-		#@userid = @userid
-		@instagram = Instagram.user_recent_media(@teste, {:count => 5})
+		#vai buscar as infos do user atraves do username
+		@teste = Instagram.user_search(params[:q], {:count => 1})
+		#saca apenas o user id dessas infos
+		@teste = @teste.first.id
+		#vai buscar as ultimas 20 fotos desse userid
+		@instagram = Instagram.user_recent_media(@teste, {:count => 20})
 	end
 
 	def perfil
